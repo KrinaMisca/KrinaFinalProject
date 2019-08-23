601928651213b847e736c28dc065076cfaf9be2e
krina misca <krinamisca99@gmail.com>
2019-08-23 13:53:47 -0400

    fixed cinemachine

--- Assets/Scripts/Managers/PlayerControler.cs
+++ Assets/Scripts/Managers/PlayerControler.cs
@@ -4,69 +4,85 @@ using UnityEngine;
 
 public class PlayerControler : MonoBehaviour
 {
-	public float jumpForce;
-	public float speed;
-	private float moveInput;
-	private bool facingRight = true;
-	private Rigidbody2D rb;
+    public float jumpForce;
+    public float speed;
+    private float moveInput;
+    private bool facingRight = true;
+    private Rigidbody2D rb;
 
     HealthBar healthBar = new HealthBar();
 
     //--------Variables for the jumping mechanics--------//
-    bool isGrounded { get { return Physics2D.Raycast(transform.position, new Vector2(0, -1), 1, LayerMask.GetMask("Ground"));} }
-	public float checkRadius;
-	public LayerMask whatIsGround;
-	private int numberOfJumps;
-	public int maxNumbeOfJumps;
-	//--------------------------------------------------//
+    bool isGrounded { get { return Physics2D.Raycast(transform.position, new Vector2(0, -1), 1, LayerMask.GetMask("Ground")); } }
+    public float checkRadius;
+    public LayerMask whatIsGround;
+    private int numberOfJumps;
+    public int maxNumbeOfJumps;
+     
+    //--------------------------------------------------//
 
-	private void Start()
-	{
-		numberOfJumps = maxNumbeOfJumps;
-		rb = GetComponent<Rigidbody2D>();
-	}
+    private void Start()
+    {
+        numberOfJumps = maxNumbeOfJumps;
+        rb = GetComponent<Rigidbody2D>();
+    }
 
     private void Update()
     {
 
-        
+
         // --------------- for the demo Test --------------- //
-        if (Input.GetKeyDown(KeyCode.C)) {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
             //HealthBar healthBar = healthBar;
-           healthBar.TakeDamage(1);
+            healthBar.TakeDamage(1);
         }
         // ------------------------------------------------- //
 
         Debug.Log(Physics2D.Raycast(transform.position, new Vector2(0, -1), 1, LayerMask.GetMask("Ground")).transform?.name);
-		if (isGrounded == true) {
-			numberOfJumps = maxNumbeOfJumps;
-		}
+        if (isGrounded == true)
+        {
+            numberOfJumps = maxNumbeOfJumps;
+        }
 
-		if (Input.GetKeyDown(KeyCode.UpArrow) && numberOfJumps > 0) {
-			rb.velocity = Vector2.up * jumpForce;
-			numberOfJumps--;
-		} 		
-	}
+        if (Input.GetKeyDown(KeyCode.UpArrow) && numberOfJumps > 0 || Input.GetKeyDown(KeyCode.Space) && numberOfJumps > 0)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+            numberOfJumps--;
+        }
+    }
+
+        //--------------------AttackTest------------------//
+        public Transform positionOfAttack;
+        public float attackRange;
+
+    
+        //------------------------------------------------//
+
 
     //physics for fixed update
-	private void FixedUpdate()
-	{
-		//--------------------LeftAndRightMovement--------------------//
-		moveInput = Input.GetAxis("Horizontal");
-		rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+    private void FixedUpdate()
+    {
+        //--------------------LeftAndRightMovement--------------------//
+        moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
 
-		if (facingRight == false && moveInput > 0) {
-			Flip();
-		} else if (facingRight == true && moveInput < 0) {
-			Flip();
-		}
-		//----------------------------------------------------------//
-	}
+        if (facingRight == false && moveInput > 0)
+        {
+            Flip();
+        }
+        else if (facingRight == true && moveInput < 0)
+        {
+            Flip();
+        }
+        //----------------------------------------------------------//
+    }
 
-	private void Flip() {
-		facingRight = !facingRight;
-		Vector3 scaler = transform.localScale;
-		scaler.x  *= -1;
-		transform.localScale = scaler;
-	}
+    private void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 scaler = transform.localScale;
+        scaler.x *= -1;
+        transform.localScale = scaler;
+    }
 }
---
+++ Assets/Scripts/Units/Enemy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int health;
+    public float speed;
+
+    private void Update()
+    {
+        if (health <= 0) {
+            Destroy(gameObject);
+        }
+    }
+
+    public void EnemyTakeDamage(int damage) {
+        health -= damage;
+        Debug.Log("Damage Taken");
+    }
+}
